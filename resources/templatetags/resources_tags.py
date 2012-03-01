2ef544818847af5c5f2d4206959afe55e288d7ba
Julian Bez <julian@freshmilk.tv>
2012-03-01 16:57:58 +0100

    Add template tag for outputting translations as list

--- resources/templatetags/resources_tags.py
+++ resources/templatetags/resources_tags.py
@@ -1,6 +1,8 @@
 from django import template
 from resources.models import Page
 from django.utils import translation
+from django.conf import settings
+from django.contrib.sites.models import Site
 
 register = template.Library()
 
@@ -18,3 +20,24 @@ def show_menu_below(context, page_pk):
                 filter(in_menu=True, language=lang)
     return {'page': context.get('page', None),
             'pages': pages}
+
+@register.inclusion_tag('resources/translations.html', takes_context=True)
+def translations(context):
+    site = Site.objects.get_current()
+    page = context['page']
+    translations = []
+    for l in settings.LANGUAGES:
+        code = subdomain = l[0]
+        v = page.translation_pool.get_by_language(code)
+        if v is not None:
+            path = v.get_absolute_url()
+        else:
+            path = "/"
+        if code == settings.LANGUAGE_CODE:
+            subdomain = "www"
+        translations.append({'code': code, 
+                             'url': "http://%s.%s%s" % (subdomain,
+                                                        site.domain,
+                                                        path),
+                             'language': l[1]})
+    return locals()
