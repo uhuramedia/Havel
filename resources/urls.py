e3959b4472d164828dbb68fafec90a5bef0ada1c
Julian Bez <julian@freshmilk.tv>
2012-08-21 14:02:05 +0200

    Make URLs flexible

--- resources/urls.py
+++ resources/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('resources.views',
-    (r'^$', 'page'),
-    (r'.*/$', 'page'),
+    url(r'^$', 'page', name="resources-single"),
+    url(r'.*/$', 'page'),
 )
