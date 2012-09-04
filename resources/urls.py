108f99b228b3fc4b7a0f189475b40e152b00d707
Julian Bez <julian@freshmilk.tv>
2012-09-04 11:55:11 +0200

    Add simple search

--- resources/urls.py
+++ resources/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('resources.views',
     (r'^$', 'page'),
+    (r'search/$', 'search'),
     (r'.*/$', 'page'),
 )
--- resources/views.py
+++ resources/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from resources.models import Resource
-from django.http import HttpResponseRedirect, HttpResponsePermanentRedirect,\
+from resources.models import Resource, Page
+from django.http import HttpResponseRedirect, HttpResponsePermanentRedirect, \
     Http404
 from django.core.urlresolvers import resolve
 from django.utils import translation
+from django.db.models import Q
 
 def page(request):
     path = request.path.strip('/').split('/')
@@ -11,3 +12,11 @@ def page(request):
     language = translation.get_language()
     resource = get_object_or_404(Resource, slug=slug, language=language)
     return resource.get_object().get_response(request)
+
+
+def search(request):
+    context = {}
+    language = translation.get_language()
+    context['q'] = q = request.GET.get('q', '')
+    context['pages'] = Page.objects.filter(Q(title__icontains=q) | Q(text__icontains=q), language=language)
+    return render(request, 'resources/search.html', context)
