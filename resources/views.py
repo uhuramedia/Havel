ea87d808078f2560936659c5336ffed6bdb58e39
Nar Kumar <nar.chhantyal@uhura.de>
2014-08-21 10:25:59 +0200

    fix search - add articles in search index if article app is installed

--- resources/views.py
+++ resources/views.py
@@ -1,7 +1,8 @@
-from django.core.urlresolvers import resolve
+from itertools import chain
+
 from django.db.models import Q
-from django.http import HttpResponseRedirect, HttpResponsePermanentRedirect, \
-    HttpResponse
+from django.conf import settings
+from django.http import HttpResponsePermanentRedirect, HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.template.base import TemplateDoesNotExist
 from django.utils import translation
@@ -25,5 +26,10 @@ def search(request):
     context = {}
     language = translation.get_language()
     context['q'] = q = request.GET.get('q', '')
-    context['pages'] = Page.objects.filter(Q(title__icontains=q) | Q(text__icontains=q), language=language)
+    pages = Page.objects.filter(Q(title__icontains=q) | Q(text__icontains=q), language=language)
+    if 'article' in settings.INSTALLED_APPS:
+        from article.models import Article
+        articles = Article.objects.filter(Q(title__icontains=q) | Q(text__icontains=q), language=language)
+        pages = list(chain(pages, articles))
+    context['pages'] = pages
     return render(request, 'resources/search.html', context)
