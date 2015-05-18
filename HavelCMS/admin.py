5486ee222d969d28c8ec8ce33a0455dab1369276
Nar Kumar <nar.chhantyal@uhura.de>
2015-05-18 15:12:48 +0200

    fix has_add_permission error

--- HavelCMS/admin.py
+++ HavelCMS/admin.py
@@ -99,7 +99,7 @@ class ResourceAdmin(FeinCMSModelAdmin):
         super(ResourceAdmin, self).__init__(*args, **kwargs)
         self.list_display_links = (None,)
 
-    def has_add_permission(self, request):
+    def has_add_permission(self, request, obj=None):
         return False
 
     def title_link(self, obj):
@@ -205,7 +205,7 @@ class WeblinkAdmin(ResourceAdmin):
             for i in getattr(settings, setting):
                 self.inlines.append(get_class_from_string(i))
 
-    def has_add_permission(self, request):
+    def has_add_permission(self, request, obj=None):
         return True
 
 admin.site.register(Weblink, WeblinkAdmin)
